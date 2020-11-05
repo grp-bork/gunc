a8f647185986f017ec574db48dbd392b7fe1c442
Anthony Noel Fullam <anthony.fullam@embl.de>
2020-11-05 16:31:29 +0100

    Reverted viz changes

--- gunc/gunc.py
+++ gunc/gunc.py
@@ -130,22 +130,8 @@ def main():
     gene_count = external_tools.get_record_count_in_fasta(diamond_inputfile)
 
     print('[INFO] Calculating scores for each tax-level:')
-    df, tax_data = chim_score(diamond_outfile, gene_count, args.sensitive)
+    df = chim_score(diamond_outfile, gene_count, args.sensitive)
     df.to_csv(f'{diamond_outfile}.chimerism_scores', index=False, sep='\t')
-
-    print('[INFO] Creating visualisation data.')
-    genome_name = tax_data['base_data']['genome'].iloc[0]
-    viz_data = visualisation.create_data(tax_data, genome_name)
-    viz_data_path = os.path.join(args.out_dir,
-                                 f'{input_basename}.viz.json')
-    with open(viz_data_path, 'w') as f:
-        f.write(viz_data)
-    print('[INFO] Creating visualisation.')
-    viz_html_path = os.path.join(args.out_dir,
-                                 f'{input_basename}.viz.html')
-    viz_html = visualisation.create_html(viz_data, genome_name)
-    with open(viz_html_path, 'w') as f:
-        f.write(viz_html)
 
 
 if __name__ == "__main__":
