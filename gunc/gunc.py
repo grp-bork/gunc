f8d74285ada923e21e21e6ebe122b21c58358182
Anthony Noel Fullam <anthony.fullam@embl.de>
2022-04-26 14:56:13 +0200

    Added option to run with a different contamination_portion cutoff

--- gunc/gunc.py
+++ gunc/gunc.py
@@ -41,6 +41,10 @@ def parse_args(args):
         "merge_checkm", help="Merge GUNC and CheckM outputs."
     )
     vis = subparsers.add_parser("plot", help="Create interactive visualisation.")
+    summarise = subparsers.add_parser(
+        "summarise",
+        help="Reevaluate results using a different contamination_portion cutoff.",
+    )
 
     run.add_argument(
         "-r",
@@ -208,6 +212,41 @@ def parse_args(args):
         help="Output directory for merged file",
         default=os.getcwd(),
         metavar="",
+    )
+    summarise.add_argument(
+        "-m",
+        "--max_csslevel_file",
+        help="MaxCSS output file from GUNC.",
+        required=True,
+        metavar="\b",
+    )
+    summarise.add_argument(
+        "-d",
+        "--gunc_detailed_output_dir",
+        help="GUNC detailed output dir.",
+        required=True,
+        metavar="\b",
+    )
+    summarise.add_argument(
+        "-c",
+        "--contamination_cutoff",
+        help="Alternatite cutoff to use.",
+        default=0.05,
+        metavar="\b",
+    )
+    summarise.add_argument(
+        "-o",
+        "--output_file",
+        help="File in which to write outputfile with added score.",
+        required=True,
+        metavar="\b",
+    )
+    summarise.add_argument(
+        "-v",
+        "--verbose",
+        help="Verbose output for debugging",
+        action="store_true",
+        default=False,
     )
     parser.add_argument(
         "-v",
@@ -730,6 +769,44 @@ def merge_checkm(args):
     merged = checkm_merge.merge_checkm_gunc(args.checkm_file, args.gunc_file)
     outfile = os.path.join(args.out_dir, "GUNC_checkM.merged.tsv")
     merged.to_csv(outfile, sep="\t", index=False)
+
+
+def get_scores_using_supplied_cont_cutoff(detail_file, cutoff=0.05):
+    df = pd.read_csv(detail_file, sep="\t", header=0)
+    df = df.drop(["pass.GUNC"], axis=1)
+    max_CSS = df.iloc[[0]].to_dict("records")[0]
+    df = df[df["taxonomic_level"] != "species"]
+    df = df[df["contamination_portion"] > cutoff]
+    if len(df) > 0:
+        max_CSSidx = df["clade_separation_score"].idxmax()
+        if not pd.isna(max_CSSidx):
+            max_CSS = df.loc[[max_CSSidx]].to_dict("records")[0]
+            if max_CSS["clade_separation_score"] > 0.45:
+                max_CSS["pass.GUNC5"] = False
+                return max_CSS
+    max_CSS["pass.GUNC5"] = True
+    return max_CSS
+
+
+def summarise(args):
+    max_csslevel_file = pd.read_csv(args.max_csslevel_file, sep="\t", header=0)
+    max_csslevel_file = max_csslevel_file.to_dict("index")
+
+    for row in max_csslevel_file:
+        pass_gunc = max_csslevel_file[row]["pass.GUNC"]
+        if pass_gunc or pd.isna(pass_gunc):
+            max_csslevel_file[row][f"pass.GUNC_{args.contamination_cutoff}"] = True
+        elif not pass_gunc:
+            bin_name = max_csslevel_file[row]["genome"]
+            detail_file = os.path.join(
+                args.gunc_detailed_output_dir, bin_name + ".all_levels.tsv"
+            )
+            new_row = get_scores_using_supplied_cont_cutoff(
+                detail_file, args.contamination_cutoff
+            )
+            max_csslevel_file[row] = new_row
+    out_df = pd.DataFrame.from_dict(max_csslevel_file, orient="index")
+    out_df.to_csv(args.output_file, index=False, sep="\t", na_rep="nan")
 
 
 def main():
